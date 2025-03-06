e18120661f87da3af2302b0b2a2165402ab33e6e
Lukas4617 <144036619+Lukas4617@users.noreply.github.com>
2025-03-06 22:07:36 +0530

    Add files via upload

---
+++ Foodie Crack/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import CategoriesHome from '../components/CategoriesHome'
+import About from '../components/About'
+import NewsLetter from '../components/NewsLetter'
+import '../styles/Home.css'
+
+const Home = () => {
+  return (
+    <div className='homepage'>
+        <Hero />
+        <CategoriesHome />
+        <NewsLetter />
+        <About />
+    </div>
+  )
+}
+
+export default Home
